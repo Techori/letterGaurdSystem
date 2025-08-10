d61f5c70ab695e8d3c4fdc7ab9c6a50472def8d4
Bhuvnesh singh <cebhuvneshsingh@gmail.com>
2025-08-11 00:35:36 +0530

    commit

--- backend/routes/staff.js
+++ backend/routes/staff.js
@@ -17,4 +17,17 @@ router.get('/', auth, async (req, res) => {
   }
 });
 
+//Delete staff by id(admin only)
+router.delete('/:id', auth, async (req, res) => {
+  try {
+    const id=req.params.id
+    const staff = await User.findByIdAndDelete({_id:id})
+      .select('-password')
+      .sort({ name: 1 });
+    res.json("Staff member(",staff.name,") deleted succesfully");
+  } catch (error) {
+    res.status(500).json({ message: 'Server error', error: error.message });
+  }
+});
+
 module.exports = router;
